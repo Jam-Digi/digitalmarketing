cba12bb5d38385e5e563b093e18f8cc856c51ce4
Jayam <153058524+Jam-Digi@users.noreply.github.com>
2023-12-07 17:51:58 +0530

    Mobile fixed

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// visually-hidden
+const hide = 'visually-hidden'
+const navHeader = document.getElementById("navHeader");
+const navlink = document.getElementById('navlink')
+
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    navHeader.classList.add(hide)
+    navlink.classList.add(hide)
+  } else {
+   navHeader.classList.remove(hide)
+   navlink.classList.remove(hide)
+  }
+}
+
+var mobileView = window.matchMedia("(max-width: 580px)");
+myFunction(mobileView); // Call listener function at run time
+mobileView.addListener(myFunction); // Attach listener function on state changes
